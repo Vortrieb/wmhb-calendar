7c5eb3c1b1d8536a6ac2b0ecde58e6b8c429bc41
Nils Riedemann <nils.riedemann@gmail.com>
2013-10-21 13:23:56 +0200

    added grunt and compass

---
+++ Gruntfile.js
@@ -0,0 +1,63 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		compass: {
+			dev: {
+				options: {
+					sassDir: 'sass',
+					cssDir: 'public/stylesheets',
+				}
+			}
+		},
+
+		express: {
+			options: {
+				script: 'app.js'
+			},
+			dev: {
+				options: {
+					script: 'app.js',
+					node_env: 'development',
+					output: 'got calendar response'
+				}
+			}
+		},
+
+		watch: {
+			express: {
+				files: [
+					'app.js', 'lib/*.js', 'lib/**/*.js'
+				],
+
+				tasks: ['express:dev'],
+
+				options: {
+					nospawn: true,
+					livereload: true
+				}
+			},
+
+			compass: {
+				files: ['sass/*.scss'],
+				tasks: ['compass']
+			},
+
+			livereload: {
+				options: {
+					livereload: true,
+				},
+				files: [
+					'public/stylesheets/*.css',
+					'views/*.mustache'
+				],
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-express-server');
+
+	grunt.registerTask('server', [ 'express:dev', 'watch' ])
+};
